8a45a9a241581dd4ddc2566f38311c7b65476014
Yici Zhu <zhu.yic@northeastern.edu>
2021-11-26 19:52:10 -0800

    Add links to previous assignments

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import './App.css';
 import './vendors/bootstrap/css/bootstrap.min.css';
 import './vendors/bootstrap/bootstrap.min.css';
 import './vendors/fontawesome/css/all.min.css';
-import Practice from "./components/a8/Practice";
-import Build from "./components/a8/Build";
+import {Link} from "react-router-dom";
+import PracticeA8 from "./components/a8/Practice";
+import PracticeA7 from "./components/a7/Practice";
+import PracticeA6 from "./components/a6/Practice";
+import BuildA8 from "./components/a8/Build";
+import BuildA7 from "./components/a7/Build";
+import BuildA6 from "./components/a6/Build";
 import {BrowserRouter, Route} from "react-router-dom";
+import HelloWorld from './components/a6/HelloWorld';
 
 
 
@@ -13,11 +19,38 @@ function App() {
       <BrowserRouter>
         <div className="container">
           <Route path={["", "/a8", "/a8/practice"]} exact={true}>
-            <Practice/>
+            <PracticeA8/>
+          </Route>
+          <Route path={["/a7", "/a7/practice"]} exact={true}>
+            <PracticeA7/>
+          </Route>
+          <Route path={["/a6", "/a6/practice"]} exact={true}>
+            <PracticeA6/>
+          </Route>
+          <Route path={["/a6/hello"]} exact={true}>
+            <HelloWorld/>
           </Route>
           <Route path="/a8/twitter/*">
-            <Build/>
+            <BuildA8/>
           </Route>
+          <Route path="/a7/twitter/*">
+            <BuildA7/>
+          </Route>
+          <Route path="/a6/twitter/*">
+            <BuildA6/>
+          </Route>
+        </div>
+        <div>
+          <h1>Previous React Assignments:</h1>
+          <Link to="/a6">
+            A6
+          </Link> |
+          <Link to="/a7">
+            A7
+          </Link> |
+          <Link to="/a8">
+            A8
+          </Link>
         </div>
       </BrowserRouter>
   );
