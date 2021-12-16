58cb5e8817f65526b6512de328615504dbd76a36
Yici Zhu <zhu.yic@northeastern.edu>
2021-12-15 20:53:55 -0800

    a6Build

--- src/components/a6/Build/index.js
+++ src/components/a6/Build/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import ExploreScreen from "./ExploreScreen";
+import Home from './HomeScreen';
+
+import {BrowserRouter, Route} from "react-router-dom";
 
 const Build = () => {
   return(
       <>
-        <ExploreScreen/>
+
+      <BrowserRouter>
+        <Route path="/a6/twitter/home">
+            <Home/>
+        </Route>
+        <Route path="/a6/twitter/explore">
+            <ExploreScreen/>
+        </Route>
+      </BrowserRouter>
         <Link to="/a6/hello">
           Hello
         </Link> |
